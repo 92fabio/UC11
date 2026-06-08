f57ab8fd1831c7065c451c921f2291689df296e9
92fabio <fabio.bala46@gmail.com>
2026-06-08 19:35:26 -0300

    validação em java

--- validação em js/validacao.js
+++ validação em js/validacao.js
@@ -1,4 +1,5 @@
 function fnValidacao() {
+ const fabio = /^[a-zA-ZÀ-ÿ0-9\s\-:,.!?()]+$/;
    const campoTituloFilme = document.getElementById("tituloFilme")
   
    if(campoTituloFilme.value.trim() == "") {
@@ -14,6 +15,15 @@ function fnValidacao() {
    if(campoTituloFilme.value.length >= 200){
     return console.log('preencha o campo titulo filme com menos de 200 caracteres')
    }
+
+   if(!fabio.test(campoTituloFilme)){
+
+    alert=("funcionou")
+    return console.log('preencha o campo titulo filme com menos de 200 caracteres')
+   }else{
+    alert=("não funcionou")
+   }
+
 
     campoTituloFilme.value.split('').map((letra) => {{
         letra = '@' ? console.log(true) : ''
